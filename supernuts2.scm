06e97cb4372c351a261acea431efd23725c71980
PM <dusthillresident@gmail.com>
2023-10-12 14:41:17 +0100

    Add files via upload

--- supernuts2.scm
+++ supernuts2.scm
@@ -5,8 +5,7 @@
 ; A simple PRNG
 ; author: dusthillresident@gmail.com https://github.com/dusthillresident/
 ;  Tested with guile, elk, scm, and 'Ikarus Scheme' interpreters.
-;  Seems to pass 'dieharder' test suite
-;  Has been tested up to 512GB in 'practrand' with no anomalies yet but further testing is needed.
+;  Currently undergoing statistical testing with 'dieharder' and 'practrand'
 ;
 ;                                Functions defined:
 ;  supernuts2 --------------------------------------------------------------------------
@@ -25,8 +24,9 @@
 ;  * Returns a list containing two numbers in the range -2147483647.0 to 2147483647.0
 
 (define supernuts2
- (let ((n1 0.0) (n2 0.0) (nlim -2147483647.0) (lim 2147483647.0)) ; n1 and n2 together constitute the current state
-  (define (fnr s) ; this is the core of supernuts2
+ (let* ((nlim -2147483647.0) (lim 2147483647.0) (n1 nlim) (n2 nlim)) ; n1 and n2 together constitute the current state
+  ; this is the main core of supernuts2
+  (define (fnr s)
    (let* ((a (* s 0.7685679659876367))
           (b (* s 0.4115346876567834))
           (v (* s 0.1178977654437879
@@ -35,18 +35,24 @@
     (if (< v 0.0)
      (- (- v (truncate v)))
      (- v (truncate v)))))
-  (define (validn? n) (and (>= n nlim) (<= n lim))) ; check if seed values are valid
+  ; this is the core of supernuts2, stage 2
+  (define (fnr2)
+   (set! n1 (+ n1 1.0))
+   (if (> n1 lim)
+    (set! n1 nlim))
+   (set! n2 (+ n2 0.73262452))
+   (if (> n2 lim)
+    (set! n2 nlim))
+   (let ((v (+ (fnr n2) (fnr n1))))
+    (- v (truncate v))))
+  ; check if seed values are valid
+  (define (validn? n) (and (>= n nlim) (<= n lim)))
+  ; the externally defined procedures of supernuts2 are created here
   (list 
 ; ---------- supernuts2 --------------------------
    (lambda ()
-    (set! n1 (+ n1 1.0))
-    (if (> n1 lim)
-     (set! n1 nlim))
-    (set! n2 (+ n2 0.73262452))
-    (if (> n2 lim)
-     (set! n2 nlim))
-    (let ((v (+ (fnr n2) (fnr n1))))
-     (- v (truncate v))))
+    (let ((v (+ (truncate (* (fnr2) 65536.0)) (* (truncate (* (fnr2) 65536.0)) 65536.0))))
+     (/ v 4294967296.0)))
 ; ---------- supernuts2-seed ---------------------
    (lambda (seed)
     (if (list? seed)
